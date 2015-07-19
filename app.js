d32dd3bff78850bbf3a7f94f80f3a49ef7f53dc2
Jon Bishop <jonbish@gmail.com>
2015-07-19 15:18:52 -0400

    remove test room update socket server emit code

--- app.js
+++ app.js
@@ -11,21 +11,20 @@ io.on('connection', function (socket) {
   console.log('Socket connection started');
 
   socket.on("connect mobile", function(){
-    socket.join('test');
     console.log('Someone entered :' + socket.id);
-    io.emit('add user', socket.id);
+    socket.broadcast.emit('add user', socket.id);
   });
 
   //Update the position
   socket.on("update movement", function(id, data){
     console.log('update position for ' +  socket.id);
-    io.emit('update position', socket.id, data);
+    socket.broadcast.emit('update position', socket.id, data);
   });
 
   //Update the state
   socket.on('update touch', function(touchevent){
-    console.log('update touch for ' +  socket.id);
-    io.emit('update state', socket.id, touchevent);
+    console.log('update state for ' +  socket.id);
+    socket.broadcast.emit('update state', socket.id, touchevent);
   });
 
   //When a user disconnects
